a3e9aee95330620a2a85c0d2e4e0c5580007a64b
bikkuclicks-arch <bikkuclicks@gmail.com>
2026-07-06 13:51:18 +0545

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBcppHmZFPt_BaHZ32BR_PgLVkzy8RFJQ0",
+  authDomain: "bikkus-studio.firebaseapp.com",
+  projectId: "bikkus-studio",
+  storageBucket: "bikkus-studio.firebasestorage.app",
+  messagingSenderId: "1006060945717",
+  appId: "1:1006060945717:web:2edfe1da68343b62aa4c7c"
 };
 
 const app = initializeApp(firebaseConfig);
